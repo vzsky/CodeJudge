8c3bdbde509e9ccf1bed54d39906b38f81f61f6d
Touch Sungkawichai <talay@10ninox.com>
2020-04-15 00:16:04 +0700

    init 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UserModule } from "./user/user.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [UserModule, MongooseModule.forRoot(process.env.MONGO_URL)],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
